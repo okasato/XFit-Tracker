ecaeb43cd189367bd11de1ff5f7abfcf5182d655
Shingo Konnai <shingokonnai@Shingos-MacBook.local>
2018-02-17 00:12:42 +0900

    Writing Top commponent

---
+++ src/Top.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+export default class Top extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const username = this.state.value;
+    fetch(`${process.env.BASE_URL}/api/users`, {
+      method: 'POST',
+      body: JSON.stringify(username),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      })
+    })
+    .then(() => console.log('Successfully create new user'))
+    .catch(err => console.log(err))
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className='top'>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.value} onChange={e => this.handleChange(e)} />
+          <input type='submit' value='submit'/>
+        </form>
+      </div>
+    )
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+// import App from './App.jsx';
+import Top from './Top.jsx';
 
 console.log(process.env.BASE_URL);
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Top />, document.getElementById('root'));
